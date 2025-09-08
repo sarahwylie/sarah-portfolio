5ab960aea1bc6ca9a16e68650adf00d547a57cbb
Sarah Wylie <sarahannemariawylie@gmail.com>
2025-09-08 12:28:32 -0500

    first commit in awhile

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-// 'use client'
 import Image from "next/image";
 import CoderGirl from "./codergirl.jpg";
 import Divider from "./divider.jpg";
+import Puppalooza from "./puppalooza.png";
+import Wildflower from "./wildflower.png";
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -52,7 +54,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-
         <div className="text-base sm:text-lg justify-items-start text-start">
           <p>
             Although I have always loved puzzles, I had a circuitous route to
@@ -84,18 +85,77 @@ export default function Home() {
             development full-time.
           </p>
         </div>
-          <div className="flex gap-4 flex-row w-fit sm:w-auto items-center justify-center">
-            <Image
-              src={Divider}
-              alt="Horizontal divider"
-              className="rounded-md text-center"
-            />
-          </div>        <br />
-        <div className="flex flex-wrap items-left justify-items-start text-start">
-          <h3 className="text-2xl sm:text-3xl font-bold italic tracking-wide">
-            Check out some of my work:
-          </h3>
+        <div className="flex gap-4 flex-row w-fit sm:w-auto items-center justify-center">
+          <Image
+            src={Divider}
+            alt="Horizontal divider"
+            className="rounded-md text-center mb-6"
+          />
         </div>
+        <div className="flex flex-wrap items-left justify-items-start text-center sm:text-start">
+          <h2 className="text-3xl sm:text-4xl font-bold italic tracking-wide mb-6">
+            Some of my recent projects
+          </h2>
+          <span>
+            I always have several things happening at once, as I tend to thrive in chaos. 
+            Check out the sites, coding puzzles, and volunteer work I've been doing lately.
+          </span>
+        </div>
+        
+        <div className="flex flex-wrap sm:flex-nowrap items-center justify-center">
+          <div>
+            <Image
+              src={Puppalooza}
+              alt="Puppalooza Screenshot"
+              className="rounded-xl"
+            />
+          </div>
+          <div className="mx-3 text-center sm:text-left">
+            <h3 className="text-2xl my-3">
+              <strong>Puppalooza</strong>
+            </h3>
+            <span>
+              <p className="my-2">
+                What to do when your foster pup turns out to be pregnant? Make a
+                website to showcase the adoptable puppies of course!
+              </p>
+            </span>
+            <span className="my-2">
+              <strong>
+                <Link href="/Puppalooza">Learn More</Link>
+              </strong>
+            </span>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap-reverse sm:flex-nowrap items-center justify-center">
+
+          <div className="mx-3 text-center sm:text-right">
+            <h3 className="text-2xl my-3">
+              <strong>Wildflower</strong>
+            </h3>
+            <span>
+              <p className="my-2">
+                I picked up WordPress while doing volunteer web maintenance work. It was a natural evolution from my Drupal experience, and after quickly learning the basics I committed to doing an accessibility and design overhaul of the landing page.
+              </p>
+            </span>
+            <span className="my-2">
+              <strong>
+                <Link href="/Wildflower">Learn More</Link>
+              </strong>
+            </span>
+          </div>
+
+          <div className="xs:order-1">
+            <Image
+              src={Wildflower}
+              alt="Wildflower website transition"
+              className="rounded-xl"
+            />
+          </div>
+
+        </div>
+
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         {/* <a
